4ba4fad4e3281e7c7d6e352ac26caf83b68a4960
unknown <daciano.batista@hotmail.com>
2018-10-17 11:47:41 -0300

    c#

---
+++ Conexao/ConexaoBD.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexao
+{
+    public class ConexaoBD
+    {
+
+		 SqlConnection SqlConnection = new SqlConnection();
+
+		public ConexaoBD()
+		{
+			SqlConnection = new SqlConnection("Server=Localhost;Database=XPTO;User Id=sa;Password=123456;");
+
+		}
+
+		public SqlConnection conector()
+		{
+			if (SqlConnection.State == System.Data.ConnectionState.Closed)
+			{
+				SqlConnection.Open();
+			}
+			return SqlConnection;
+		}
+
+		public void desconectar()
+		{
+
+			if (SqlConnection.State == System.Data.ConnectionState.Open)
+			{
+				SqlConnection.Close();
+			}
+
+		
+		}
+
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Conexao;
+using EmpresaXPTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	static class Program
+	{
+		
+		static void Main()
+		{
+
+			LeitorTxt leitor = new LeitorTxt();
+
+
+			leitor.salvarClienteBD();
+			leitor.SalvarProdutosBD();
+			leitor.exbirClienteAtivo();
+
+			int count = 0;
+
+			foreach (Cliente cliente in leitor.clientesAtivos)
+			{
+				Console.WriteLine("Cliente: "+cliente.Nome);
+				if (cliente.produtos != null)
+				{
+
+					Console.WriteLine("Teste " + cliente.produtos.Count());
+					while (count < cliente.produtos.Count())
+					{
+						Console.WriteLine("Nome: " + cliente.Nome);
+						Console.WriteLine("Produtos: " + cliente.produtos[count].Descricao);
+						count++;
+					}
+				}
+				count = 0;
+				
+			}
+
+		}
+
+		
+
+	}
+}
